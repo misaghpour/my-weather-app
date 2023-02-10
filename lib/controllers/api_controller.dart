a2ecde2298154049a297e7807dd4530e76903228
M. Misaghpour <m.misaghpour@gmail.com>
2023-02-10 08:36:34 +0330

    minor security changes

--- lib/controllers/api_controller.dart
+++ lib/controllers/api_controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:my_weather_app/data/models/forecast_model.dart';
 import 'package:my_weather_app/data/models/weather_mode.dart';
@@ -6,7 +7,9 @@ class APIController extends GetxController {
   final _connect = GetConnect();
 
   Future<WeatherModel> fetchCurrentCity({required String city}) async {
-    final api_key = '52a28657ca36b07c3a80b5f8901981ff';
+    // final api_key = '52a28657ca36b07c3a80b5f8901981ff';
+    await dotenv.load();
+    final api_key = dotenv.env['API_KEY'];
     final url =
         'https://api.openweathermap.org/data/2.5/weather?q=$city&appid=$api_key&units=metric';
     final response = await _connect.get(url);
@@ -16,7 +19,9 @@ class APIController extends GetxController {
   }
 
   Future<ForecastModel> fetchForecast({required String city}) async {
-    final api_key = '52a28657ca36b07c3a80b5f8901981ff';
+    // final api_key = '52a28657ca36b07c3a80b5f8901981ff';
+    await dotenv.load();
+    final api_key = dotenv.env['API_KEY'];
     final url =
         'https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$api_key&units=metric';
     final response = await _connect.get(url);
